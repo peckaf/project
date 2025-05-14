2a08f9544d99ffb51f7bf324ec99477534156c65
peckaf <maksimbelza8@gmail.com>
2025-05-14 17:35:32 +0300

    Update TokyoFixBEN.lua

--- TokyoFixBEN.lua
+++ TokyoFixBEN.lua
@@ -4763,7 +4763,7 @@ function library:CreateSettingsTab(menu)
     local themeSection = settingsTab:AddSeparator('Custom Theme');
     local setByPreset = false
 
-        setByPreset = true
+        setByPreset = false
         for _,v in next, library.themes do
             if v.name == newTheme then
                 for x, d in pairs(library.options) do
