6f379e9fe3393931525cce7c3f942502815e5a4a
peckaf <maksimbelza8@gmail.com>
2025-05-14 14:58:50 +0300

    Update librarywithnogui.lua

--- librarywithnogui.lua
+++ librarywithnogui.lua
@@ -58,15 +58,6 @@ local library = {
         ['colorhue'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/lgbtqshit.png';
         ['colortrans'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/trans.png';
     };
-    numberStrings = {['Zero'] = 0, ['One'] = 1, ['Two'] = 2, ['Three'] = 3, ['Four'] = 4, ['Five'] = 5, ['Six'] = 6, ['Seven'] = 7, ['Eight'] = 8, ['Nine'] = 9};
-    signal = loadstring(game:HttpGet('https://raw.githubusercontent.com/drillygzzly/Other/main/1414'))();
-    open = false;
-    opening = false;
-    hasInit = false;
-    cheatname = startupArgs.cheatname or 'Clanware';
-    gamename = startupArgs.gamename or 'Universal';
-    fileext = startupArgs.fileext or '.txt';
-}
 
 library.themes = {
     {
